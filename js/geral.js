10276c956fc2ad23ee082b0f5873f85a408d9d45
Mauricio Cepinho <61973062+maucepinho@users.noreply.github.com>
2024-06-13 19:11:19 -0300

    Update geral.js

--- js/geral.js
+++ js/geral.js
@@ -1,4 +1,4 @@
-const urlbase = "http://localhost:3030";
+const urlbase = "http://https://abp-goldenhives.onrender.com:3030";
 //const urlbase = "https://abp-1dsm-server.onrender.com";
 // Variável usada para manter os dados do usuário logado
 // A função carregarLogin verifica se o usuário está logado e carrega na variável usuarioLogado
